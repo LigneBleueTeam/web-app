c41b3c62e3a900238b6ab57adcb48b4845e0df84
Amine <amine.bms24@gmail.com>
2020-01-15 15:23:37 +0100

    blank fix

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -52,12 +52,12 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
+    <a href="{{ route('home') }}" class="brand-link">
       <img src="{{ asset('assets/img/AdminLTELogo.png') }}"
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLigneBleue</span>
+      <span class="brand-text font-weight-light"><b>Admin</b>LigneBleue</span>
     </a>
 
     <!-- Sidebar -->
@@ -86,14 +86,14 @@
             </a>
           </li>
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
+            <a href="#" class="nav-link" style="background-color: rgba(255,255,255,.1);">
               <i class="nav-icon fas fa-users"></i>
               <p>
                 Utilisateurs
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
+            <ul class="nav nav-treeview menu-open" style="display:block;">
               <li class="nav-item">
                 <a href="{{ route('users') }}" class="nav-link">
                   <i class="fas fa-th nav-icon"></i>
@@ -110,14 +110,14 @@
           </li>
 
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
+            <a href="#" class="nav-link" style="background-color: rgba(255,255,255,.1);">
               <i class="nav-icon fas fa-snowboarding"></i>
               <p>
                 Activités
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
+            <ul class="nav nav-treeview menu-open" style="display:block;">
               <li class="nav-item">
                 <a href="{{ route('activities') }}" class="nav-link">
                   <i class="fas fa-th nav-icon"></i>
@@ -134,14 +134,14 @@
           </li>
 
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
+            <a href="#" class="nav-link" style="background-color: rgba(255,255,255,.1);">
               <i class="nav-icon fas fa-chart-line"></i>
               <p>
                 Programmes
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
-            <ul class="nav nav-treeview">
+            <ul class="nav nav-treeview menu-open" style="display:block;">
               <li class="nav-item">
                 <a href="{{ route('programs') }}" class="nav-link">
                   <i class="fas fa-th nav-icon"></i>
@@ -156,7 +156,14 @@
               </li>
             </ul>
           </li>
-
+          <li class="nav-item">
+            <a href="{{ route('logout') }}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Se déconnecter
+              </p>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
